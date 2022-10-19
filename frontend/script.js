9da1b164c1e6489b0edc233137824cf61b56ca85
Daniel Lendvai <lendvai.daniel92@gmail.com>
2022-10-19 11:42:50 +0200

    datalist logic added

--- frontend/script.js
+++ frontend/script.js
@@ -8,27 +8,22 @@ rootElement.insertAdjacentHTML(
     `
 <h2 class="city-name"></h2>
 <input list="cities" name="city" id="cityInput">
+<datalist id="cities">
+<option value="">
+<option value="">
+<option value="">
+<option value="">
+<option value="">
+<option value="">
+</datalist>
 <input type="submit">
 `
 );
 const cityName = document.querySelector(".city-name");
 const cityNameInput = document.querySelector("#cityInput");
 const btn = document.querySelector("button");
-
-cityNameInput.insertAdjacentHTML(
-    "afterend",
-    `
-    <datalist id="cities">
-        <option value="">
-        <option value="">
-        <option value="">
-        <option value="">
-        <option value="">
-    </datalist>
-    `
-);
-
-let options = document.querySelectorAll("option");
+const dataList = document.querySelector("datalist");
+const options = document.querySelectorAll("option");
 
 cityNameInput.addEventListener("input", (e) => {
     cityNames = [];
@@ -45,9 +40,23 @@ cityNameInput.addEventListener("input", (e) => {
         .then((response) => response.json())
         .then((data) => {
             data.forEach((l) => {
-                cityNames.push(l);
+                    cityNames.push(l);
             });
         })
         .catch(() => {});
-        cityName.textContent = e.target.value;
+            cityName.textContent = e.target.value;
+            fillOptions();
 });
+
+function fillOptions() {
+    setTimeout(() => { 
+    for(let i=0; i<options.length;i++){
+            options[0].value = cityNames[0].name;
+            options[1].value = cityNames[1].name;
+            options[2].value = cityNames[2].name;
+            options[3].value = cityNames[3].name;
+            options[4].value = cityNames[4].name;
+            options[5].value = cityNames[5].name;
+    }
+}, "1000");
+}
