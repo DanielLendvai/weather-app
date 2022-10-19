4f9acf00872a8d4fe318f695d6b7f3f9f8281df3
Daniel Lendvai <lendvai.daniel92@gmail.com>
2022-10-19 08:53:21 +0200

    comments added

--- frontend/script.js
+++ frontend/script.js
@@ -4,15 +4,12 @@ rootElement.insertAdjacentHTML(
     `
 <h1 class="city-name"></h1>
 <input type="text" class="city-name-input">
-<datalist class="country-list">
-</datalist>
 <button></button>
 `
 );
 const cityName = document.querySelector(".city-name");
 const cityNameInput = document.querySelector(".city-name-input");
 const btn = document.querySelector("button");
-const dataList = document.querySelector("datalist");
 
 let cityNames = [];
 
@@ -31,31 +28,28 @@ let cityNames = [];
         .then((data) => {})
         .catch(() => {});
 } */
-cityNameInput.addEventListener("input", (e) => {
-    fetch(
-        `http://api.weatherapi.com/v1/search.json?key=c03e30c3acf1486cb5674845221710&q=${e.target.value}`,
-        {
-            Connection: "keep-alive",
-            Vary: "Accept-Encoding",
-            "Content-Length": "2334",
-            "Content-Type": "text/html",
-            Date: "Mon, 17 Oct 2022 08:16:41 GMT",
-        }
-    )
-        .then((response) => response.json())
-        .then((data) => {
-          data.forEach((c) => {
-            if(!cityNames.includes(c)) {
-              cityNames.push(c)
-            }
-          })
-            /* for (let i = 0; i < data.length; i++) {
-                if (!cityNames.includes(data[i].name)) 
-                cityNames.push(data[i]);
-            } */
+//input change method, add timeout, clear the array, datalist-> https://www.w3schools.com/tags/tag_datalist.asp
 
-        })
-        .catch(() => {});
+cityNameInput.addEventListener("input", (e) => {
+        fetch(
+            `http://api.weatherapi.com/v1/search.json?key=c03e30c3acf1486cb5674845221710&q=${e.target.value}`,
+            {
+                Connection: "keep-alive",
+                Vary: "Accept-Encoding",
+                "Content-Length": "2334",
+                "Content-Type": "text/html",
+                Date: "Mon, 17 Oct 2022 08:16:41 GMT",
+            }
+        )
+            .then((response) => response.json())
+            .then((data) => {
+              data.forEach((c) => {
+                if(!cityNames.find(city => city.name === c)) {
+                  cityNames.push(c)
+                }
+              })
+            })
+            .catch(() => {});
     cityName.textContent = e.target.value;
     console.log(cityNames);
 });
