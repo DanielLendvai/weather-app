8a3368bfed49a84ef3348a128953c50154033c8f
capali13 <mark.csaszarnagy@gmail.com>
2022-10-19 11:47:57 +0200

    desing

--- frontend/script.js
+++ frontend/script.js
@@ -3,21 +3,14 @@ let cityNames = [];
 
 //input change method, add timeout, clear the array, datalist-> https://www.w3schools.com/tags/tag_datalist.asp
 
-rootElement.insertAdjacentHTML(
-    "beforeend",
-    `
-<h2 class="city-name"></h2>
-<input list="cities" name="city" id="cityInput">
-<input type="submit">
-`
-);
+rootElement.insertAdjacentHTML("beforeend");
 const cityName = document.querySelector(".city-name");
 const cityNameInput = document.querySelector("#cityInput");
 const btn = document.querySelector("button");
 
 cityNameInput.insertAdjacentHTML(
-    "afterend",
-    `
+  "afterend",
+  `
     <datalist id="cities">
         <option value="">
         <option value="">
@@ -31,23 +24,23 @@ cityNameInput.insertAdjacentHTML(
 let options = document.querySelectorAll("option");
 
 cityNameInput.addEventListener("input", (e) => {
-    cityNames = [];
-    fetch(
-        `http://api.weatherapi.com/v1/search.json?key=c03e30c3acf1486cb5674845221710&q=${e.target.value}`,
-        {
-            Connection: "keep-alive",
-            Vary: "Accept-Encoding",
-            "Content-Length": "2334",
-            "Content-Type": "text/html",
-            Date: "Mon, 17 Oct 2022 08:16:41 GMT",
-        }
-    )
-        .then((response) => response.json())
-        .then((data) => {
-            data.forEach((l) => {
-                cityNames.push(l);
-            });
-        })
-        .catch(() => {});
-        cityName.textContent = e.target.value;
+  cityNames = [];
+  fetch(
+    `http://api.weatherapi.com/v1/search.json?key=c03e30c3acf1486cb5674845221710&q=${e.target.value}`,
+    {
+      Connection: "keep-alive",
+      Vary: "Accept-Encoding",
+      "Content-Length": "2334",
+      "Content-Type": "text/html",
+      Date: "Mon, 17 Oct 2022 08:16:41 GMT",
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((l) => {
+        cityNames.push(l);
+      });
+    })
+    .catch(() => {});
+  cityName.textContent = e.target.value;
 });
